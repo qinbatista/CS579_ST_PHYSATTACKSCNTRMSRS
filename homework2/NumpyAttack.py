aed6eecd7a94cab6a631e1d2d05d91798da21ff1
qin.batista <qin.batista@outlook.com>
2023-01-27 22:44:37 -0800

    update

--- homework2/NumpyAttack.py
+++ homework2/NumpyAttack.py
@@ -7,21 +7,21 @@ from matplotlib import pyplot as plt
 class DataManager:
     def __init__(self, path):
         self._timer = TimerTool()
-        self.__data = np.memmap(path, dtype='uint8', mode='r')
-        self.__n = self.__data.shape[0]
+        self._data = np.memmap(path, dtype='uint8', mode='r')
+        self.__n = self._data.shape[0]
         pass
 
     def _naive_approach_mean(self):
-        sum = self.__data.sum()
-        mean = sum / self.__data.shape[0]
+        sum = self._data.sum()
+        mean = sum / self._data.shape[0]
         print(f"[Naive Approach]Mean:		{mean}")
         return mean
 
     def _naive_approach_variance(self):
-        sum = self.__data.sum()
-        mean = sum / self.__data.shape[0]
-        squared_differences = (self.__data-mean)**2
-        variance = squared_differences.sum() / self.__data.shape[0]
+        sum = self._data.sum()
+        mean = sum / self._data.shape[0]
+        squared_differences = (self._data-mean)**2
+        variance = squared_differences.sum() / self._data.shape[0]
         print(f"[Naive Approach]Variance:	{variance}")
         return variance
 
@@ -39,10 +39,48 @@ class DataManager:
         print(f"[Welford Algorithm]Variance:	{variance}")
         return variance
 
+    def _one_pass(self, data_list):
+        self._timer._start()
+        mean = 0
+        variance = 0
+        mean_variance = 0
+        M2 = 0
+        n = 0
+        for data in data_list:
+            n += 1
+            mean += (data - mean) / n
+
+            delta = (data - mean_variance)
+            mean_variance += delta / n
+            M2 += delta * (data - mean_variance)
+        variance = M2 / n
+        self._timer._stop()
+        self._timer._display()
+        print(f"[One Pass]Mean:			{mean}")
+        print(f"[One Pass]Variance:		{variance}")
+
+    def _histogram_method(self, data):
+        self._timer._start()
+        hist, bin_edges = np.histogram(data, bins=3)
+        # Calculate the mean
+        mean = np.sum(hist * bin_edges[:-1]) / data.shape[0]
+        variance = np.sum((bin_edges[:-1]-mean)**2)/data.shape[0]
+        self._timer._stop()
+        self._timer._display()
+        print(f"[Histogram Method]Histogram Mean:	{mean}")
+        print(f"[Histogram Method]Histogram Variance:	{variance}")
+
 
 if __name__ == '__main__':
     myDataManager = DataManager('measurement_data_2023_uint8.bin')
-    mean = myDataManager._naive_approach_mean()
-    variance = myDataManager._naive_approach_variance()
-    new_mean = myDataManager._welford_algorithm_mean(mean, 122)
-    new_variance = myDataManager._welford_algorithm_variance(new_mean, variance, 122)
+    # print("----------naive approach-----------")
+    # mean = myDataManager._naive_approach_mean()
+    # variance = myDataManager._naive_approach_variance()
+    # print("----------welford algorithm-----------")
+    # new_mean = myDataManager._welford_algorithm_mean(mean, 122)
+    # new_variance = myDataManager._welford_algorithm_variance(new_mean, variance, 122)
+    a = myDataManager._data[:100000]
+    print("----------One Pass-----------")
+    myDataManager._one_pass(a)
+    print("----------Histogram Method-----------")
+    myDataManager._histogram_method(a)
--- homework2/TimerTool.py
+++ homework2/TimerTool.py
@@ -10,7 +10,7 @@ class TimerTool:
         self.__startTime = 0
         self.__executionTime = 0
         self.__number_of_tasks = 0
-        self._timerStart()
+        self._start()
         self._cores_count = psutil.cpu_count()
         print("----------🛠️ Your System Information------")
         print("🖥️  	System: ", platform.system(), "		|")
@@ -18,28 +18,28 @@ class TimerTool:
         print("🎛️  	CPU cores: ", psutil.cpu_count(), "			|")
         print("💾  	RAM: ", psutil.virtual_memory().total / (1024.0 ** 3), "GB", "			|")
         print("-----------------------------------------\n")
-        self._timerStop()
+        self._stop()
 
     def _maxCoreProcessing(self, function, values):
         self.__number_of_tasks = len(values)
         print(f"----------📥 MaxCoreProcessing Started-----------")
         print(f"⛏️ 	Function Name:{function.__name__}		|")
         print(f"📦 	Number of Tasks:{str(self.__number_of_tasks)}			|")
-        self._timerStart()
+        self._start()
         with Pool() as p:
             p.map(function, values)
         pass
-        self._timerStop()
+        self._stop()
         print(f"----------✅ MaxCoreProcessing Ended-------------\n")
-        self._displayExecutionTime()
+        self._display()
 
-    def _timerStart(self):
+    def _start(self):
         self.__startTime = time.time()
 
-    def _timerStop(self):
+    def _stop(self):
         self.__executionTime = time.time() - self.__startTime
 
-    def _displayExecutionTime(self):
+    def _display(self):
         print("----------🛠️  Execution Information-------------------------------")
         print("🚀 Execution Time ("+str(psutil.cpu_count())+" cores used):	" + str(self.__executionTime)+"'s	|")
         print("🎛️  Each Core's Execution Time:		" + str(self.__executionTime/psutil.cpu_count())+"'s	|")
@@ -48,4 +48,4 @@ class TimerTool:
 
 if __name__ == '__main__':
     myHackTool = TimerTool()
-    myHackTool._displayExecutionTime()
+    myHackTool._display()
